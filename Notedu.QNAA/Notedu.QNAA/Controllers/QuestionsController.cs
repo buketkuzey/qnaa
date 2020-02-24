3139f83f4862452f5376b45982aa365e2559ab8c
İren SALTALI <iren@saltali.com>
2020-02-24 23:20:45 +0300

    Intial commit

---
+++ Notedu.QNAA/Notedu.QNAA/Controllers/QuestionsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Notedu.QNAA.Models;
+
+namespace Notedu.QNAA.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class QuestionsController : ControllerBase
+    {
+
+        private readonly ILogger<QuestionsController> _logger;
+
+        public QuestionsController(ILogger<QuestionsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Question> Get()
+        {
+            return new List<Question>();
+        }
+    }
+}

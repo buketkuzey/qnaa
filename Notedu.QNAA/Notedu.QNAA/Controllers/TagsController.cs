e2138ea4071a4b0efe03759554f21869d6ef887c
İren SALTALI <iren@saltali.com>
2020-02-24 23:48:39 +0300

    Tags Controller is added

---
+++ Notedu.QNAA/Notedu.QNAA/Controllers/TagsController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Notedu.QNAA.Models;
+
+namespace Notedu.QNAA.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TagsController : ControllerBase
+    {
+
+        private readonly ILogger<TagsController> _logger;
+
+        public TagsController(ILogger<TagsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public TagModel Get(int id)
+        {
+            return new TagModel();
+        }
+
+        [HttpPost]
+        public TagModel Create(TagModel model)
+        {
+            return new TagModel();
+        }
+
+        [HttpPost]
+        public TagModel Update(TagModel model)
+        {
+            return new TagModel();
+        }
+
+        [HttpDelete]
+        public TagModel Delete(int id)
+        {
+            return new TagModel();
+        }
+    }
+}
--- Notedu.QNAA/Notedu.QNAA/Models/AnswerModel.cs
+++ Notedu.QNAA/Notedu.QNAA/Models/AnswerModel.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Notedu.QNAA.Models
+﻿namespace Notedu.QNAA.Models
 {
     public class AnswerModel
     {
